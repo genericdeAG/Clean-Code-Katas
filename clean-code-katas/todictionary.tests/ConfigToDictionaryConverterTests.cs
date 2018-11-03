59d1ba737aee40a5d871a02c8c9a021c14480a4a
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-03 09:24:10 +0100

    ToDictionary: Create failing test for SplitIntoSettings

--- clean-code-katas/todictionary.tests/ConfigToDictionaryConverterTests.cs
+++ clean-code-katas/todictionary.tests/ConfigToDictionaryConverterTests.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using FluentAssertions;
 using Xunit;
 
@@ -6,5 +6,30 @@ namespace todictionary.tests
 {
     public class ConfigToDictionaryConverterTests
     {
+        private readonly ConfigToDictionaryConverter _target;
+
+        public ConfigToDictionaryConverterTests()
+        {
+            _target = new ConfigToDictionaryConverter();
+        }
+
+        [Fact]
+        public void SplitIntoSettings_GivenCsvConfig_ShouldReturnSettings()
+        {
+            var csv = "a=1;b=2;c=3;d=4;";
+            var expected = new[] {"a=1", "b=2", "c=3", "d=4"};
+
+            var result = _target.SplitIntoSettings(csv);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+
+    internal class ConfigToDictionaryConverter
+    {
+        public IEnumerable SplitIntoSettings(string csv)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
