a6ea3dd0e721b7b48ceb6ed0946c0ae39b28a698
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-09 17:29:24 +0100

    FromRomanNumerals: Create failing test for MapRomanDigitsToDecimalDigits

--- clean-code-katas/function-katas/tests/fromromannumerals.tests/RomanToDecimalConverterTests.cs
+++ clean-code-katas/function-katas/tests/fromromannumerals.tests/RomanToDecimalConverterTests.cs
@@ -1,15 +1,39 @@
-﻿namespace fromromannumerals.tests
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace fromromannumerals.tests
 {
     public class RomanToDecimalConverterTests
     {
         private readonly RomanToDecimalConverter _target;
+
         public RomanToDecimalConverterTests()
         {
             _target = new RomanToDecimalConverter();
+        }
+
+        [Theory]
+        [InlineData("IV", 1, 5)]
+        [InlineData("XXVI", 10, 10, 5, 1)]
+        [InlineData("MDC", 1000, 500, 100)]
+        [InlineData("L", 50)]
+        [InlineData("V", 5)]
+        [InlineData("III", 1, 1, 1)]
+        [InlineData("VII", 5, 1, 1)]
+        public void MapRomanDigitsToDecimalDigits_GivenRomanNumber_ShouldReturnDecimalDigits(string romanNumber, params int[] decimalDigits)
+        {
+            var actual = _target.MapRomanDigitsToDecimalDigits(romanNumber);
+
+            decimalDigits.Should().BeEquivalentTo(actual);
         }
     }
 
     internal class RomanToDecimalConverter
     {
+        public IEnumerable<int> MapRomanDigitsToDecimalDigits(string romanNumber)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
