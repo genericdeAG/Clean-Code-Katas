78c945b1c4858d1a016b0a2f15222e718f12a099
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-09 17:27:20 +0100

    FromRomanNumerals: Create RomanToDecimalConverter test class

---
+++ clean-code-katas/function-katas/tests/fromromannumerals.tests/RomanToDecimalConverterTests.cs
@@ -0,0 +1,15 @@
+﻿namespace fromromannumerals.tests
+{
+    public class RomanToDecimalConverterTests
+    {
+        private readonly RomanToDecimalConverter _target;
+        public RomanToDecimalConverterTests()
+        {
+            _target = new RomanToDecimalConverter();
+        }
+    }
+
+    internal class RomanToDecimalConverter
+    {
+    }
+}
