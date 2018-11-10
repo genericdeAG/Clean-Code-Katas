5fa7baf58477ee675fb52f07f5f1b2629d69addf
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-10 15:52:16 +0100

    FizzBuzz: Create failing test for ConvertToFizzBuzzText

--- clean-code-katas/function-katas/tests/fizzbuzz.tests/FizzBuzzConverterTests.cs
+++ clean-code-katas/function-katas/tests/fizzbuzz.tests/FizzBuzzConverterTests.cs
@@ -1,7 +1,34 @@
-﻿namespace fizzbuzz.tests
+﻿using FluentAssertions;
+using Xunit;
+
+namespace fizzbuzz.tests
 {
     public class FizzBuzzConverterTests
     {
-        
+        private readonly FizzBuzzConverter _target;
+        public FizzBuzzConverterTests()
+        {
+            _target = new FizzBuzzConverter();
+        }
+
+        [Theory]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(15, "FizzBuzz")]
+        public void ConvertToFizzBuzzText_GivenNumber_ShouldReturnFizzBuzzText(int number, string fizzBuzzText)
+        {
+            var actual = _target.ConvertToFizzBuzzText(number);
+
+            fizzBuzzText.Should().Be(actual);
+        }
+    }
+
+    internal class FizzBuzzConverter
+    {
+        internal string ConvertToFizzBuzzText(int number)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
