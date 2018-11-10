f0e51e7e9d8b0e55e723de4bf693167353364eb9
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-10 17:20:05 +0100

    FizzBuzz: Make DoFizzBuzz test green
    Implement functionality to make DoFizzBuzz test pass

--- clean-code-katas/function-katas/tests/fizzbuzz.tests/FizzBuzzTests.cs
+++ clean-code-katas/function-katas/tests/fizzbuzz.tests/FizzBuzzTests.cs
@@ -15,16 +15,7 @@ namespace fizzbuzz.tests
         [Fact]
         public void DoFizzBuzz_ShouldReturn_FizzBuzzOutput()
         {
-            var expected = @"FizzBuzz,1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,
-                            11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,
-                            Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,
-                            31,32,Fizz,34,Buzz,Fizz,37,38,Fizz,Buzz,
-                            41,Fizz,43,44,FizzBuzz,46,47,Fizz,49,Buzz,
-                            Fizz,52,53,Fizz,Buzz,56,Fizz,58,59,FizzBuzz,
-                            61,62,Fizz,64,Buzz,Fizz,67,68,Fizz,Buzz,
-                            71,Fizz,73,74,FizzBuzz,76,77,Fizz,79,Buzz,
-                            Fizz,82,83,Fizz,Buzz,86,Fizz,88,89,FizzBuzz,
-                            91,92,Fizz,94,Buzz,Fizz,97,98,Fizz,100";
+            var expected = @"1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32,Fizz,34,Buzz,Fizz,37,38,Fizz,Buzz,41,Fizz,43,44,FizzBuzz,46,47,Fizz,49,Buzz,Fizz,52,53,Fizz,Buzz,56,Fizz,58,59,FizzBuzz,61,62,Fizz,64,Buzz,Fizz,67,68,Fizz,Buzz,71,Fizz,73,74,FizzBuzz,76,77,Fizz,79,Buzz,Fizz,82,83,Fizz,Buzz,86,Fizz,88,89,FizzBuzz,91,92,Fizz,94,Buzz,Fizz,97,98,Fizz,Buzz";
 
             _target.DoFizzBuzz();
 
@@ -34,11 +25,26 @@ namespace fizzbuzz.tests
 
     internal class FizzBuzz
     {
+        private readonly NumberGenerator _numberGenerator;
+        private readonly FizzBuzzConverter _fizzBuzzConverter;
+        private readonly TextDisplay _textDisplay;
+        private readonly Action<string> _displayAction;
         public string Output { get; set; }
+
+        public FizzBuzz()
+        {
+            _numberGenerator = new NumberGenerator();
+            _fizzBuzzConverter = new FizzBuzzConverter();
+            _textDisplay = new TextDisplay();
+            _displayAction = (t) => Output = t;
+
+        }
 
         public void DoFizzBuzz()
         {
-            throw new System.NotImplementedException();
+            var numbers = _numberGenerator.GenerateNumbers();
+            var fizzBuzzTexts = _fizzBuzzConverter.ConvertToFizzBuzzTexts(numbers);
+            _textDisplay.DisplayTexts(fizzBuzzTexts, _displayAction);
         }
     }
 }
