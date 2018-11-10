14603776e3c7e75a79c05b52edf5dd415ad9c228
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-10 16:19:48 +0100

    FizzBuzz: Create NumberGenerator test class

---
+++ clean-code-katas/function-katas/tests/fizzbuzz.tests/NumberGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿namespace fizzbuzz.tests
+{
+    public class NumberGeneratorTests
+    {
+        private readonly NumberGenerator _target;
+
+        public NumberGeneratorTests()
+        {
+            _target = new NumberGenerator();
+        }
+    }
+
+    internal class NumberGenerator
+
+    {
+    }
+}
