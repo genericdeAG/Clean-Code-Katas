f901787777bc97b38e2df553100adf21b19608c6
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-10 16:23:44 +0100

    FizzBuzz: Create failing test for GenerateNumbers

--- clean-code-katas/function-katas/tests/fizzbuzz.tests/NumberGeneratorTests.cs
+++ clean-code-katas/function-katas/tests/fizzbuzz.tests/NumberGeneratorTests.cs
@@ -1,4 +1,9 @@
-﻿namespace fizzbuzz.tests
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace fizzbuzz.tests
 {
     public class NumberGeneratorTests
     {
@@ -8,10 +13,23 @@
         {
             _target = new NumberGenerator();
         }
+
+        [Fact]
+        public void GenerateNumbers_ShouldReturnNumbersFrom1To100()
+        {
+            var expected = Enumerable.Range(0, 100);
+
+            var actual = _target.GenerateNumbers();
+
+            expected.Should().BeEquivalentTo(actual);
+        }
     }
 
     internal class NumberGenerator
-
     {
+        public IEnumerable<int> GenerateNumbers()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
