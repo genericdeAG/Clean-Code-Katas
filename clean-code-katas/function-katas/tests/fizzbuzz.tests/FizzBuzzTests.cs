647da0a688b24ea65f2bcc16926a7d6079cf5201
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-10 17:10:54 +0100

    FizzBuzz: Create failing test for DoFizzBuzz

--- clean-code-katas/function-katas/tests/fizzbuzz.tests/FizzBuzzTests.cs
+++ clean-code-katas/function-katas/tests/fizzbuzz.tests/FizzBuzzTests.cs
@@ -1,4 +1,8 @@
-﻿namespace fizzbuzz.tests
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace fizzbuzz.tests
 {
     public class FizzBuzzTests
     {
@@ -7,9 +11,34 @@
         {
             _target = new FizzBuzz();
         }
+
+        [Fact]
+        public void DoFizzBuzz_ShouldReturn_FizzBuzzOutput()
+        {
+            var expected = @"FizzBuzz,1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,
+                            11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,
+                            Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,
+                            31,32,Fizz,34,Buzz,Fizz,37,38,Fizz,Buzz,
+                            41,Fizz,43,44,FizzBuzz,46,47,Fizz,49,Buzz,
+                            Fizz,52,53,Fizz,Buzz,56,Fizz,58,59,FizzBuzz,
+                            61,62,Fizz,64,Buzz,Fizz,67,68,Fizz,Buzz,
+                            71,Fizz,73,74,FizzBuzz,76,77,Fizz,79,Buzz,
+                            Fizz,82,83,Fizz,Buzz,86,Fizz,88,89,FizzBuzz,
+                            91,92,Fizz,94,Buzz,Fizz,97,98,Fizz,100";
+
+            _target.DoFizzBuzz();
+
+            expected.Should().BeEquivalentTo(_target.Output);
+        }
     }
 
     internal class FizzBuzz
     {
+        public string Output { get; set; }
+
+        public void DoFizzBuzz()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
