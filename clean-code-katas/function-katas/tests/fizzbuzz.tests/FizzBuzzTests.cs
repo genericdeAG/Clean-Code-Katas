c02f44e7fd79e8279ec711811c0178ba226436eb
Nicolai Pischel <nicolai.pischel@generic.de>
2018-11-10 16:51:28 +0100

    FizzBuzz: Create FizzBuzz test class

---
+++ clean-code-katas/function-katas/tests/fizzbuzz.tests/FizzBuzzTests.cs
@@ -0,0 +1,15 @@
+﻿namespace fizzbuzz.tests
+{
+    public class FizzBuzzTests
+    {
+        private readonly FizzBuzz _target;
+        public FizzBuzzTests()
+        {
+            _target = new FizzBuzz();
+        }
+    }
+
+    internal class FizzBuzz
+    {
+    }
+}
