147709e6da2b6fbf861d4c8767bea7d4e210e590
Nicolai Pischel <nicolai.pischel@live.de>
2019-09-21 09:34:14 +0200

    Providing an additional fizz buzz implementation with extension methods

---
+++ clean-code-katas/fizzbuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    internal sealed class Program
+    {
+        internal static void Main(string[] args)
+        {
+            GenerateNumbers(0, 100).ConvertToFizzBuzzTexts().DisplayTexts((Console.WriteLine));
+        }
+
+        internal static IEnumerable<int> GenerateNumbers(int rangeStart, int count)
+        {
+            return Enumerable.Range(rangeStart, count);
+        }
+    }
+}
