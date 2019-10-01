dc355e18e6000544d8c702a5160668f613068d0f
Nicolai Pischel <nicolai.pischel@live.de>
2019-10-01 23:43:34 +0200

    Creating adapter for the CLI

---
+++ clean-code-katas/library-katas/file_io_fromromannumerals/adapters/cli/CommandLineInterfaceAdapter.cs
@@ -0,0 +1,12 @@
+﻿namespace adapters.cli
+{
+    using contracts;
+
+    public class CommandLineInterfaceAdapter : ICommandLineInterface
+    {
+        public string GetFilePathFromParameters(string[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
