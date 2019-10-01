ed71db89382551bd74aad6c31385c16c883e97b7
Nicolai Pischel <nicolai.pischel@live.de>
2019-10-01 22:47:06 +0200

    Implementing tests for the happy path for the roman numeral converter

---
+++ clean-code-katas/library-katas/tests/file_io_fromromannumerals.tests/RomanToDecimalConverterTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace file_io_fromromannumerals.tests
+{
+    using core;
+    using FluentAssertions;
+    using Xunit;
+
+    public class RomanToDecimalConverterTests
+    {
+        [Theory]
+        [InlineData("IV", 4)]
+        [InlineData("XXVI", 26)]
+        [InlineData("MDC", 1600)]
+        [InlineData("L", 50)]
+        [InlineData("V", 5)]
+        [InlineData("III", 3)]
+        [InlineData("VII", 7)]
+        public void Should_ConvertToDecimalNumber_Given_ValidRomanNumeral(string romanNumeral, int expectedDecimalNumber)
+        {
+            var actual = RomanToDecimalConverter.ConvertToDecimalNumber(romanNumeral);
+
+            expectedDecimalNumber.Should().Be(actual);
+        }
+    }
+}
