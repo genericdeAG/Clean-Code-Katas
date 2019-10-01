165ebf9e1e5a93079e7faa3e8611b2862e8928c0
Nicolai Pischel <nicolai.pischel@live.de>
2019-10-01 21:15:10 +0200

    Creating FileService for reading and writing contents to files

---
+++ clean-code-katas/library-katas/file_io_fromromannumerals/providers/FileService.cs
@@ -0,0 +1,18 @@
+﻿namespace providers
+{
+    using System.Collections.Generic;
+    using contracts;
+
+    public class FileService : IWriteToFile, IReadFromFile
+    {
+        public void Write(string content)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<string> Read(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
