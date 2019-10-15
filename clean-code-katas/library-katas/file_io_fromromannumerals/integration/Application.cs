25f23079a9b714745d94b511bb84f3e8cd8a4353
Nicolai Pischel <nicolai.pischel@live.de>
2019-10-15 19:39:02 +0200

    Implementing failing tests for the application

---
+++ clean-code-katas/library-katas/file_io_fromromannumerals/integration/Application.cs
@@ -0,0 +1,27 @@
+﻿namespace integration
+{
+    using System;
+    using contracts;
+
+    public class Application
+    {
+        private readonly IFileSystem _persistence;
+        private readonly IUserInterface _ui;
+        private readonly ICommandLineInterface _cli;
+
+        public Application(
+            IFileSystem persistence,
+            IUserInterface ui,
+            ICommandLineInterface cli)
+        {
+            _persistence = persistence;
+            _ui = ui;
+            _cli = cli;
+        }
+
+        public void Execute(string[] args, Action<string> display)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ clean-code-katas/library-katas/tests/file_io_fromromannumerals.tests/ApplicationTests.cs
@@ -0,0 +1,41 @@
+﻿namespace file_io_fromromannumerals.tests
+{
+    using contracts;
+    using contracts.Dtos;
+    using integration;
+    using NSubstitute;
+    using Xunit;
+
+    public class ApplicationTests
+    {
+        private readonly IUserInterface _uiStub;
+        private readonly IFileSystem _persistenceStub;
+        private readonly ICommandLineInterface _cliStub;
+
+        private readonly Application _target;
+        private const string FilePath = @"romanNumerals.txt";
+
+        public ApplicationTests()
+        {
+            _uiStub = Substitute.For<IUserInterface>();
+            _persistenceStub = Substitute.For<IFileSystem>();
+            _cliStub = Substitute.For<ICommandLineInterface>();
+
+            _target = new Application(_persistenceStub, _uiStub, _cliStub);
+
+            _persistenceStub.Read(Arg.Any<string>()).Returns(FileDto.Create(
+                new[] { "MDLXXXII", "CLXXXIII", "LVIII", "MMXIV" }));
+            _cliStub.GetFilePathFromParameters(Arg.Any<string[]>()).Returns(FilePath);
+        }
+
+        [Fact]
+        public void AcceptanceTest()
+        {
+            var args = new[] { FilePath };
+
+            const string expectedOutput = "1582, 183, 58, 2014";
+            _target.Execute(args, _uiStub.DisplayMessage);
+            _uiStub.Received(1).DisplayMessage(expectedOutput);
+        }
+    }
+}
