b46738dd2c7a96426835a85ee5b9ecf799be35fa
Nicolai Pischel <nicolai.pischel@live.de>
2019-10-15 20:00:57 +0200

    Implementing host environment for the application

---
+++ clean-code-katas/library-katas/file_io_fromromannumerals/host/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace host
+{
+    using adapters.cli;
+    using adapters.persistence;
+    using adapters.ui;
+    using integration;
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ui = new UserInterfaceAdapter();
+            var persistence = new FileSystemAdapter();
+            var cli = new CommandLineInterfaceAdapter();
+            var app = new Application(persistence, ui, cli);
+
+            app.Execute(args, ui.DisplayMessage);
+        }
+    }
+}
