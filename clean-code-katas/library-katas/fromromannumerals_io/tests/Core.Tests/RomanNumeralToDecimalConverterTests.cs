880911ba8de8f7dde71db13bcc618fef441033b2
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-06 22:52:03 +0100

    Implementing the functionality to make the test pass

--- clean-code-katas/library-katas/fromromannumerals_io/tests/Core.Tests/RomanNumeralToDecimalConverterTests.cs
+++ clean-code-katas/library-katas/fromromannumerals_io/tests/Core.Tests/RomanNumeralToDecimalConverterTests.cs
@@ -22,10 +22,20 @@ namespace Core.Tests
 
         public static class RomanNumeralToDecimalConverter
         {
+            private static readonly Dictionary<char, int> RomanDecimalMapping = new Dictionary<char, int>
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
 
             internal static IEnumerable<int> ConvertToDecimalDigits(string romanNumeral)
             {
-                throw new NotImplementedException();
+                return romanNumeral.Select(digit => RomanDecimalMapping[digit]);
             }
         }
     }
