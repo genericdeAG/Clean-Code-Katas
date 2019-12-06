a0e1f096e6d77015b876da30e38892aa4d9bde60
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-06 22:51:03 +0100

    Implementing failing test for the roman numeral to decimal digit conversion

--- clean-code-katas/library-katas/fromromannumerals_io/tests/Core.Tests/RomanNumeralToDecimalConverterTests.cs
+++ clean-code-katas/library-katas/fromromannumerals_io/tests/Core.Tests/RomanNumeralToDecimalConverterTests.cs
@@ -1,3 +1,6 @@
+using System;
+using Xunit;
+
 namespace Core.Tests
 {
     using System.Collections;
@@ -7,5 +10,23 @@ namespace Core.Tests
 
     public class RomanNumeralToDecimalConverterTests
     {
+        [Theory]
+        [InlineData("X", new[] { 10 })]
+        [InlineData("XI", new[] { 10, 1 })]
+        [InlineData("IX", new [] { 1, 10 })]
+        public void ConvertToDecimalDigits_Should_ConvertListOfRomanNumerals_ToItsDecimalDigits(string romanNumeral, int[] decimalDigits)
+        {
+            var result = RomanNumeralToDecimalConverter.ConvertToDecimalDigits(romanNumeral);
+            result.Should().BeEquivalentTo(decimalDigits);
+        }
+
+        public static class RomanNumeralToDecimalConverter
+        {
+
+            internal static IEnumerable<int> ConvertToDecimalDigits(string romanNumeral)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
