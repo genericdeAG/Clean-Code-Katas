a8d7e60d66a8127a7badf373d2316d79d4dc21c3
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-07 10:46:37 +0100

    Moving the Application to its own assembly

---
+++ clean-code-katas/library-katas/fromromannumerals_io/integration/Application.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace integration
+{
+    using System.Linq;
+    using contracts;
+    using core;
+
+    public class Application
+    {
+        private readonly IPersistence _persistence;
+        private readonly IUserInterface _ui;
+        private readonly ICommandLineInterface _cli;
+
+        public Application(
+            IPersistence persistence,
+            IUserInterface ui,
+            ICommandLineInterface cli)
+        {
+            _persistence = persistence;
+            _ui = ui;
+            _cli = cli;
+        }
+
+        public void Run(string[] args)
+        {
+            var filePath = _cli.GetFilePathFromParameters(args);
+            var romanNumerals = _persistence.GetRomanNumerals(filePath);
+            var decimalNumbers = romanNumerals.Select(RomanNumeralToDecimalConverter.ConvertToDecimalNumber).ToArray();
+            _persistence.SaveResult(filePath, decimalNumbers);
+            _ui.ShowResult(decimalNumbers);
+        }
+    }
+}
--- clean-code-katas/library-katas/fromromannumerals_io/tests/fromromannumerals_io.Tests/IntegrationTests.cs
+++ clean-code-katas/library-katas/fromromannumerals_io/tests/fromromannumerals_io.Tests/IntegrationTests.cs
@@ -45,30 +45,4 @@
             _ui.Received(1).ShowResult(_dummyDecimalNumbers);
         }
     }
-
-    public class Application
-    {
-        private readonly IPersistence _persistence;
-        private readonly IUserInterface _ui;
-        private readonly ICommandLineInterface _cli;
-
-        public Application(
-            IPersistence persistence,
-            IUserInterface ui,
-            ICommandLineInterface cli)
-        {
-            _persistence = persistence;
-            _ui = ui;
-            _cli = cli;
-        }
-
-        public void Run(string[] args)
-        {
-            var filePath = _cli.GetFilePathFromParameters(args);
-            var romanNumerals = _persistence.GetRomanNumerals(filePath);
-            var decimalNumbers = romanNumerals.Select(RomanNumeralToDecimalConverter.ConvertToDecimalNumber).ToArray();
-            _persistence.SaveResult(filePath, decimalNumbers);
-            _ui.ShowResult(decimalNumbers);
-        }
-    }
 }
