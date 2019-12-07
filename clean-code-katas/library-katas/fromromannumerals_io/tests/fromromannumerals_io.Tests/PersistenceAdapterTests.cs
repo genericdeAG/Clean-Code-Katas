ce2e9f5fa6cd35683da7e987297b13f80a107d3e
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-07 08:27:04 +0100

    Creating failing test for retrieving roman numerals from the file system

--- clean-code-katas/library-katas/fromromannumerals_io/tests/fromromannumerals_io.Tests/PersistenceAdapterTests.cs
+++ clean-code-katas/library-katas/fromromannumerals_io/tests/fromromannumerals_io.Tests/PersistenceAdapterTests.cs
@@ -4,8 +4,39 @@ using System.Text;
 
 namespace fromromannumerals_io.Tests
 {
+    using contracts;
+    using FluentAssertions;
+    using Xunit;
+
     public class PersistenceAdapterTests
     {
+        private readonly IPersistence _persistence;
 
+        public PersistenceAdapterTests()
+        {
+            _persistence = new PersistenceAdapter();
+        }
+
+        [Fact]
+        public void GetRomanNumerals_Should_RetrieveRomanNumerals_FromGivenFilePath()
+        {
+            var expected = new[] { "MMMCMXCIX", "CCCXXX", "DCCCLXXX", "D", "MCMXCIV" };
+            var filePath = @"C:\SRC\Clean-Code-Katas\clean-code-katas\library-katas\fromromannumerals_io\tests\fromromannumerals_io.Tests\files\romannumerals.txt";
+            var actual = _persistence.GetRomanNumerals(filePath);
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+
+    public class PersistenceAdapter : IPersistence
+    {
+        public IEnumerable<string> GetRomanNumerals(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveResult(IEnumerable<int> decimalNumbers)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
