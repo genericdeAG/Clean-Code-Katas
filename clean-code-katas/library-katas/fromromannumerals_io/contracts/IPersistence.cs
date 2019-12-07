c9e2319ab0664a20a6bff909f81cf06c9ff0c3de
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-07 07:31:40 +0100

    Creating the contract for the persistence

---
+++ clean-code-katas/library-katas/fromromannumerals_io/contracts/IPersistence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace contracts
+{
+    public interface IPersistence
+    {
+        IEnumerable<string> GetRomanNumerals(string filePath);
+
+        void SaveResult(IEnumerable<int> decimalNumbers);
+    }
+}
