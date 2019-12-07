dd5253acf56a0a128c53e98c0ffc56468b29e3d5
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-07 10:52:18 +0100

    Implementing the host to create and start the application process

--- clean-code-katas/library-katas/fromromannumerals_io/host/Program.cs
+++ clean-code-katas/library-katas/fromromannumerals_io/host/Program.cs
@@ -2,11 +2,21 @@
 
 namespace host
 {
+    using cli;
+    using integration;
+    using persistence;
+    using ui;
+
     public class Program
     {
         static void Main(string[] args)
         {
+            var ui = new UserInterfaceAdapter(Console.WriteLine);
+            var persistence = new PersistenceAdapter();
+            var cli = new CommandLineInterfaceAdapter();
+            var app = new Application(persistence, ui, cli);
 
+            app.Run(args);
         }
     }
 }
