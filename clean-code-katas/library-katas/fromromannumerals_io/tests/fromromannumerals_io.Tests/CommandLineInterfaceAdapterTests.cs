dcecd41d38c88f2ab52503ebe40d659eb9c5b9c3
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-07 09:23:40 +0100

    Creating failing tests for reading command-line arguments

--- clean-code-katas/library-katas/fromromannumerals_io/tests/fromromannumerals_io.Tests/CommandLineInterfaceAdapterTests.cs
+++ clean-code-katas/library-katas/fromromannumerals_io/tests/fromromannumerals_io.Tests/CommandLineInterfaceAdapterTests.cs
@@ -1,10 +1,51 @@
 ﻿namespace fromromannumerals_io.Tests
 {
+    using System;
+    using System.Linq;
     using contracts;
+    using FluentAssertions;
     using Xunit;
 
     public class CommandLineInterfaceAdapterTests
     {
-        
+        private readonly ICommandLineInterface _cli;
+
+        public CommandLineInterfaceAdapterTests()
+        {
+            _cli = new CommandLineInterfaceAdapter();
+        }
+
+        [Fact]
+        public void GetFilePathFromParameters_Should_RetrieveFilePath_Given_Arguments()
+        {
+            var expected = "testFilePath";
+            var args = new[] { expected };
+            var actual = _cli.GetFilePathFromParameters(args);
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GetFilePathFromParameters_Should_ReturnNull_Given_EmptyArguments()
+        {
+            var args = Enumerable.Empty<string>().ToArray();
+            var actual = _cli.GetFilePathFromParameters(args);
+            actual.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetFilePathFromParameters_Should_ReturnFirstArgument_Given_MultipleArguments()
+        {
+            var args = new[] {"foo", "bar"};
+            var actual = _cli.GetFilePathFromParameters(args);
+            actual.Should().Be("foo");
+        }
+    }
+
+    public class CommandLineInterfaceAdapter : ICommandLineInterface
+    {
+        public string GetFilePathFromParameters(string[] args)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
