b1c94d6a53af4d6c5fbfd4c64915670ccf3917e9
Nicolai Pischel <nicolai.pischel@live.de>
2019-12-07 09:56:49 +0100

    Implementing the user interface adapter

---
+++ clean-code-katas/library-katas/fromromannumerals_io/adapters/ui/UserInterfaceAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ui
+{
+    using System.Collections.Generic;
+    using contracts;
+
+    public class UserInterfaceAdapter : IUserInterface
+    {
+        private readonly Action<string> _displayAction;
+
+        public UserInterfaceAdapter(Action<string> displayAction)
+        {
+            _displayAction = displayAction;
+        }
+
+        public void ShowResult(IEnumerable<int> decimalNumbers)
+        {
+            foreach (var number in decimalNumbers)
+            {
+                _displayAction(number.ToString());
+            }
+        }
+    }
+}
